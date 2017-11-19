cf9fded36a90c8313e9b028f697b17b30b92266e
Julio Olivera <julio.olvr@gmail.com>
2017-11-19 18:41:06 -0500

    Make request to flights API

--- bot.go
+++ bot.go
@@ -1,7 +1,46 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"os"
+)
+
+type Flight struct {
+	price int
+}
+
+type FlightResponse struct {
+	Conversion struct {
+		Usd int
+	}
+	Dtimeutc int
+	Atimeutc int
+}
+
+type ApiResponse struct {
+	Data []FlightResponse
+}
 
 func main() {
-	fmt.Printf("Bot.\n")
+	url := "https://api.skypicker.com/flights?flyFrom=JFK&to=LAX&dateFrom=01/01/2018&dateTo=10/01/2018&daysInDestinationFrom=10&daysInDestinationTo=15&curr=USD"
+
+	resp, err := http.Get(url)
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error making request %s\n", err)
+		return
+	}
+
+	var response ApiResponse
+
+	err = json.NewDecoder(resp.Body).Decode(&response)
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error decoding response %s\n", err)
+		return
+	}
+
+	fmt.Printf("Response %v\n", response)
 }
